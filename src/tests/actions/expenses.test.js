28c7befeb32da994f01e40a3ca541c115b5b1c83
swathi <swathiraj815@gmail.com>
2021-01-17 22:53:52 -0600

    Set up edit expense in database

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
   removeExpense,
   setExpenses,
   startSetExpenses, 
-  startRemoveExpense} from '../../actions/expenses';
+  startRemoveExpense,
+  startEditExpense} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -53,6 +54,26 @@ test('Should setup edit expense object', () => {
     type: 'EDIT_EXPENSE',
     id: '123abc',
     updates: {note: "This is swathi test"}
+  })
+})
+
+test('Should edit expense object in the firebase', (done) => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = {
+    amount: 21030
+  }
+  store.dispatch( startEditExpense(id,updates)).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type:'EDIT_EXPENSE',
+      id,
+      updates
+    });
+    return database.ref(`expenses/${id}`).once('value');
+  }).then((snapshot) => {
+    expect(snapshot.val().amount).toBe(updates.amount);
+    done();
   })
 })
 
